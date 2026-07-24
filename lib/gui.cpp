331bed97895c42a771b96158e8f3b2a5f782cd92
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-07-24 17:25:04 +0200

    Modified CMake project layout

--- lib/gui.cpp
+++
@@ -1,9 +0,0 @@
-#include <wx/wx.h>
-
-namespace ApplicationExample {
-  class Application : public wxApp {
-    auto OnInit() -> bool override {return (new wxFrame {nullptr, wxID_ANY, wxEmptyString})->Show();}
-  };
-}
-
-wxIMPLEMENT_APP(ApplicationExample::Application);
