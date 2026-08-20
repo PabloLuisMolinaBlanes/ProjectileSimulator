f0dc076631019dc2d994f8852bfaef2cdcf029e6
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-08-20 17:31:35 +0200

    Added test for acceleration in phys engine

--- tests/src/phys_engine_tests/phys_engine_tests.cpp
+++ tests/src/phys_engine_tests/phys_engine_tests.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include "math.h"
 #include "phys_engine.hpp"
 
 TEST(SampleParticleMovesProperlyII, BasicAssertions) {
@@ -29,4 +30,19 @@ TEST(SampleParticleMovesProperlyIII, BasicAssertions) {
     float result = simulateProjectile(*state, 0.2, 1.0)[4].initial_position_x;
     // Assert
     EXPECT_EQ(1.0, result);
+}
+
+TEST(SampleParticleMovesProperlyIV, BasicAssertions) {
+    // Arrange
+    State * state = (State *) malloc(sizeof(State));
+    state->initial_position_x = 0;
+    state->initial_velocity_x = 1.0;
+    state->acceleration_x = 1.0;
+    state->initial_position_y = 0;
+    state->initial_velocity_y = 0;
+    state->acceleration_y = 0;
+    // Act
+    float result = simulateProjectile(*state, 1.0, 5.0)[4].initial_position_x;
+    // Assert
+    EXPECT_EQ(17.5, result);
 }
